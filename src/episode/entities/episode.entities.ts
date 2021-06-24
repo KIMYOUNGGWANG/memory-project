07ddaf0480d702edfd45b5c7e76ee79a024a4e3a
maintainker <syhwan88@naver.com>
2021-06-24 13:42:19 +0900

    [feat] copy entities

---
+++ src/episode/entities/episode.entities.ts
@@ -0,0 +1,48 @@
+import { ObjectType, Field, InputType } from '@nestjs/graphql';
+import { Entity, Column, OneToMany } from 'typeorm';
+import { CoreEntity } from 'src/common/core.entity';
+// import { GroupUser } from './group-user.entity';
+import { Story } from 'src/story/entities/story.entity';
+// import { User } from 'src/user/entities/user.entity';
+
+@InputType('groupSpaceInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class GroupSpace extends CoreEntity {
+  @Field(() => String)
+  @Column()
+  name: string;
+
+  @Field(() => String, { nullable: true, defaultValue: '' })
+  @Column({ default: '' })
+  description: string;
+
+  @Field(() => String)
+  @Column()
+  type: string; // 돈냈냐?
+
+  @Field(() => String)
+  @Column()
+  coverImg: string;
+
+  @Field(() => Boolean, { defaultValue: false })
+  @Column({ default: false })
+  is_deleted: boolean;
+
+  // @OneToMany(() => GroupUser, groupUser => groupUser.group, {
+  //   nullable: true,
+  //   onDelete: 'SET NULL',
+  // })
+  // @Field(() => [GroupUser], { nullable: true })
+  // groupUser: GroupUser[];
+
+  @OneToMany(() => Story, story => story.groupSpace, {
+    onDelete: 'SET NULL',
+  })
+  @Field(() => [Story], { nullable: true })
+  story: Story[];
+
+  // @Field(() => GroupUser)
+  // @OneToOne(()=>GroupUser,{on})
+  // created_by: GroupUser;
+}
