9984fbf364cf64af3ba3c8cbca1355215483ed3d
maintainker <syhwan88@naver.com>
2021-06-27 14:04:09 +0900

    [feat] module 추가

--- src/episode/episode.module.ts
+++ src/episode/episode.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GroupSpace } from 'src/group/entities/group.entity';
 import { GroupUser } from 'src/group/entities/group-user.entity';
+import { EpisodeResolver } from './episode.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([GroupSpace, GroupUser])],
-  providers: [],
+  providers: [EpisodeResolver],
   exports: [],
 })
 export class EpisodeModule {}
