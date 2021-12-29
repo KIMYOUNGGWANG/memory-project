41c7618ef8469d33e1927fbaad57b91a4160ba9b
maintainker <syhwan88@naver.com>
2021-12-29 12:38:22 +0900

    [setting] front app setting

--- src/user/user.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const UserPermission = (permissions: string[]) =>
-  SetMetadata('permission', permissions);
-
-export const GroupPermission = (permissions: string[]) =>
-  SetMetadata('permission', permissions);
-///
