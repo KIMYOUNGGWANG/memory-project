6ffa8b79258d02d6f7a7d1eec8eb77678ca42c16
maintainker <syhwan88@naver.com>
2022-01-05 08:35:06 +0900

    [config] booting

---
+++ src/context/index.tsx
@@ -0,0 +1,9 @@
+import { QueryClient } from "react-query";
+
+const getInitialData = async (queryClient: QueryClient) => {
+  //await queryClient.fetchQuery(queryKey, queryFn)
+  try {
+  } catch (error) {}
+};
+
+export default getInitialData;
--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { QueryClient } from "react-query";
+import getInitialData from "./context";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const boot = async () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 3000,
+      },
+    },
+  });
+  await getInitialData(queryClient);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root"),
+  );
+  reportWebVitals();
+};
+
+boot();
