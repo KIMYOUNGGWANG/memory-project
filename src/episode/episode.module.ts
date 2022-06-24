98cdf16f97c75cacf30329c34b9e0fa4fccc881f
maintainker <syhwan88@naver.com>
2022-06-25 03:48:48 +0900

    copy 중

--- src/episode/episode.module.ts
+++ src/episode/episode.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GroupSpace } from 'src/group/entities/group.entity';
+import { GroupUser } from 'src/group/entities/group-user.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([GroupSpace, GroupUser])],
+  providers: [GroupResolver, GroupService],
+  exports: [GroupService],
+})
 export class EpisodeModule {}
---
+++ src/episode/episode.resolver.ts
@@ -0,0 +1,37 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+// import { GroupSpace } from './entities/group.entity';
+// import { CreateGroupInput, CreateGroupOutput } from './dtos/create-group.dto';
+import { AuthUser } from 'src/auth/auth.decorator';
+import { User } from 'src/user/entities/user.entity';
+// import { EditGroupOutput, EditGroupInput } from './dtos/edit-group.dto';
+import { UserPermission } from 'src/user/user.decorator';
+import { GroupService } from 'src/group/group.service';
+
+@Resolver(() => GroupSpace)
+export class EpisodeResolver {
+  constructor(private readonly groupSevice: GroupService) {}
+  @Query(() => Boolean)
+  hi() {
+    return true;
+  }
+
+  // @Mutation(() => CreateGroupOutput)
+  // @UserPermission(['any'])
+  // createGroup(
+  //   @Args('input') createGroupInput: CreateGroupInput,
+  //   @AuthUser() user: User,
+  // ): Promise<CreateGroupOutput> {
+  //   return this.groupSevice.createGroup(createGroupInput, user);
+  // }
+
+  // @Mutation(() => EditGroupOutput)
+  // @UserPermission(['any'])
+  // editGroup(
+  //   @Args('input') editGroupInput: EditGroupInput,
+  //   @AuthUser() user: User,
+  // ) {
+  //   return {
+  //     ok: true,
+  //   };
+  // }
+}
